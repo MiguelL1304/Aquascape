a7625b0ccccc164bafdcd016f3adf61255aee274
Miguel Leon <mleon6@ggc.edu>
2024-11-05 14:55:29 -0500

    Linked db with fish generation

--- Aquascape/app/Stacks/home/AquariumScreen.js
+++ Aquascape/app/Stacks/home/AquariumScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { ScrollView, Image, StyleSheet, Dimensions, View, Text, TouchableOpacity } from 'react-native';
 import Animated, {
   useSharedValue,
@@ -6,11 +6,10 @@ import Animated, {
   withTiming,
   withRepeat,
 } from 'react-native-reanimated';
-
-//Firebase Imports
 import { auth } from '../../../firebase/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { firestoreDB } from '../../../firebase/firebase';
+import { useFocusEffect } from '@react-navigation/native';
 
 // Importing Colors and Elements for styling
 import Colors from '../../../constants/Colors';
@@ -20,117 +19,59 @@ const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
 const AquariumScreen = ({ navigation }) => {
-  const [uid, setUid] = useState(null);
-
-  const [aquariumData, setAquariumData] = useState(null);
-  //const [fishArray, setFishArray] = useState([]);
-
-  useEffect(() => {
-    const fetchAquariumData = async () => {
-      const user = auth.currentUser;
-      if (user) {
-        const uid = user.uid;
-        const aquariumRef = doc(firestoreDB, "aquarium", uid);
-
-        try {
-          const aquariumSnap = await getDoc(aquariumRef);
-          if (aquariumSnap.exists()) {
-            const data = aquariumSnap.data(); // Assign aquariumSnap.data() to `data`
-            setAquariumData(data);
-
-            // const fishes = generateFishArray(data); // Use `data` here
-            // setFishArray(fishes);
-
-          } else {
-            console.log("No aquarium data found");
-          }
-        } catch (error) {
-          console.error("Error fetching aquarium data: ", error);
-        }
-      }
-    };
-
-    fetchAquariumData();
-  }, []);
-  
-  // Array of fish with randomized initial positions, directions, distances, and durations
-  const fishArray = [
-    {
-      id: 1,
-      offset: useSharedValue(Math.random() * (screenWidth * 3)),
-      direction: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      targetDistance: Math.random() * (screenWidth * 2 - screenWidth) + screenWidth,
-      duration: Math.random() * 6000 + 5000,
-      scaleX: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      positionY: Math.random() * (screenHeight - 120), // Random vertical position within the screen height
-      image: require('../../../assets/Pufferfish.gif'),
-    },
-    {
-      id: 2,
-      offset: useSharedValue(Math.random() * (screenWidth * 3)),
-      direction: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      targetDistance: Math.random() * (screenWidth * 2 - screenWidth) + screenWidth,
-      duration: Math.random() * 6000 + 5000,
-      scaleX: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      positionY: Math.random() * (screenHeight - 120),
-      image: require('../../../assets/StaticShark.gif'),
-    },
-    {
-      id: 3,
-      offset: useSharedValue(Math.random() * (screenWidth * 3)),
-      direction: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      targetDistance: Math.random() * (screenWidth * 2 - screenWidth) + screenWidth,
-      duration: Math.random() * 6000 + 5000,
-      scaleX: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      positionY: Math.random() * (screenHeight - 120),
-      image: require('../../../assets/catfish-export.gif'),
-    },
-    {
-      id: 4,
-      offset: useSharedValue(Math.random() * (screenWidth * 3)),
-      direction: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      targetDistance: Math.random() * (screenWidth * 2 - screenWidth) + screenWidth,
-      duration: Math.random() * 6000 + 5000,
-      scaleX: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      positionY: Math.random() * (screenHeight - 120),
-      image: require('../../../assets/bluetang-export.gif'),
-    },
-    {
-      id: 5,
-      offset: useSharedValue(Math.random() * (screenWidth * 3)),
-      direction: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      targetDistance: Math.random() * (screenWidth * 2 - screenWidth) + screenWidth,
-      duration: Math.random() * 6000 + 5000,
-      scaleX: useSharedValue(Math.random() > 0.5 ? 1 : -1),
-      positionY: Math.random() * (screenHeight - 120),
-      image: require('../../../assets/goldfish-export.gif'),
-    },
-    // Add more fish objects as needed
-  ];
+  const [fishArray, setFishArray] = useState(Array(15).fill(null).map((_, index) => ({
+    id: index + 1,
+    offset: useSharedValue(Math.random() * (screenWidth * 3)),
+    direction: useSharedValue(Math.random() > 0.5 ? 1 : -1),
+    targetDistance: Math.random() * (screenWidth * 2 - screenWidth) + screenWidth,
+    duration: Math.random() * 6000 + 5000,
+    scaleX: useSharedValue(Math.random() > 0.5 ? 1 : -1),
+    positionY: Math.random() * (screenHeight - 120),
+    image: null,  // Placeholder for the image, will be set based on fetched data
+  })));
 
   const imageMap = {
     "Pufferfish.gif": require("../../../assets/Pufferfish.gif"),
     "StaticShark.gif": require("../../../assets/StaticShark.gif"),
+    "goldfish-export.gif": require("../../../assets/goldfish-export.gif"),
     // Add more images here as needed
   };
 
-  // const generateFishArray = (aquariumData) => {
-  //   return aquariumData.fish.map((fish, index) => ({
-  //     id: index + 1,
-  //     targetDistance: Math.random() * (screenWidth * 2 - screenWidth) + screenWidth,
-  //     duration: Math.random() * 6000 + 5000,
-  //     positionY: Math.random() * (screenHeight - 120),
-  //     image: imageMap[fish.fileName] || null, // Use imageMap to get the correct image source
-  //     // Hooks like `useSharedValue` will be added in the component later
-  //   }));
-  // };
+  // Fetch the aquarium data each time the screen is focused
+  useFocusEffect(
+    useCallback(() => {
+      const fetchAquariumData = async () => {
+        const user = auth.currentUser;
+        if (user) {
+          const uid = user.uid;
+          const aquariumRef = doc(firestoreDB, "aquarium", uid);
 
-  // Initialize each fish's scaleX based on direction so it faces the correct way initially
-  fishArray.forEach(fish => {
-    fish.scaleX.value = fish.direction.value === 1 ? 1 : -1;
-  });
+          try {
+            const aquariumSnap = await getDoc(aquariumRef);
+            if (aquariumSnap.exists()) {
+              const fishData = aquariumSnap.data().fish;
+              console.log(fishData);
 
-  // Animated styles for each fish based on their direction and offset
+              // Map fetched fish data to update the image in fishArray
+              setFishArray(prevFishArray => prevFishArray.map((fish, index) => {
+                const fetchedFish = fishData[index];
+                return fetchedFish
+                  ? { ...fish, image: imageMap[fetchedFish.fileName] || null }
+                  : fish;
+              }));
+            } else {
+              console.log("No aquarium data found");
+            }
+          } catch (error) {
+            console.error("Error fetching aquarium data: ", error);
+          }
+        }
+      };
+
+      fetchAquariumData();
+    }, [])
+  );
+
   const fishStyles = fishArray.map(fish =>
     useAnimatedStyle(() => {
       return {
@@ -138,69 +79,44 @@ const AquariumScreen = ({ navigation }) => {
           { translateX: fish.offset.value },
           { scaleX: fish.scaleX.value },
         ],
-        top: fish.positionY, // Position each fish at a random vertical position
+        top: fish.positionY,
       };
     })
   );
 
   useEffect(() => {
-    const user = auth.currentUser;
-    if (user) {
-      setUid(user.uid);
-    }
-
     fishArray.forEach(fish => {
-      // Calculate initial target position
       const targetPosition = fish.direction.value === 1
         ? Math.min(fish.offset.value + fish.targetDistance, screenWidth * 3)
         : Math.max(fish.offset.value - fish.targetDistance, 0);
 
-      // Start repeating animation
       fish.offset.value = withRepeat(
         withTiming(targetPosition, { duration: fish.duration }, () => {
-          // Toggle direction and flip scaleX when direction changes
           fish.direction.value *= -1;
-          fish.scaleX.value *= -1; // Flip the image
+          fish.scaleX.value *= -1;
         }),
-        -1, // Infinite repetition
-        true // Alternate direction on each repeat
+        -1,
+        true
       );
     });
-  }, []);
-
-  // Handle the store button press
-  const handleStore = () => {
-    navigation.navigate("Store");
-  };
+  }, [fishArray]);
 
   return (
     <View style={{ flex: 1 }}>
-      <ScrollView
-        style={styles.scrollContainer}
-        horizontal={true}
-        showsHorizontalScrollIndicator={true}
-      >
+      <ScrollView style={styles.scrollContainer} horizontal={true} showsHorizontalScrollIndicator={true}>
         <View style={styles.bannerContainer}>
-          <Image 
-            source={require('../../../assets/backgroundSample.png')} 
-            style={styles.banner}
-            resizeMode="cover"
-          />
-          {/* Map through fish array and render each Animated fish */}
+          <Image source={require('../../../assets/backgroundSample.png')} style={styles.banner} resizeMode="cover" />
           {fishArray.map((fish, index) => (
-            <Animated.View key={fish.id} style={[styles.box, fishStyles[index]]}>
-              <Image
-                source={fish.image}
-                style={styles.boxImage}
-                resizeMode="contain"
-              />
-            </Animated.View>
+            fish.image && (
+              <Animated.View key={fish.id} style={[styles.box, fishStyles[index]]}>
+                <Image source={fish.image} style={styles.boxImage} resizeMode="contain" />
+              </Animated.View>
+            )
           ))}
         </View>
       </ScrollView>
-
-      {/* Store button using mainButton styles from Elements */}
-      <TouchableOpacity style={[Elements.mainButton, styles.storeButton]} onPress={handleStore}>
+       {/* Store button using mainButton styles from Elements */}
+      <TouchableOpacity style={[Elements.mainButton, styles.storeButton]} onPress={() => navigation.navigate("Store")}>
         <Text style={Elements.mainButtonText}>Store</Text>
       </TouchableOpacity>
     </View>
@@ -212,7 +128,7 @@ export default AquariumScreen;
 const styles = StyleSheet.create({
   scrollContainer: {
     flex: 1,
-    backgroundColor: Colors.lightBlue,
+    backgroundColor: '#ADD8E6',
   },
   bannerContainer: {
     height: screenHeight,
@@ -228,7 +144,6 @@ const styles = StyleSheet.create({
     width: 120,
     borderRadius: 20,
     position: 'absolute',
-    top: screenHeight / 2 - 60,
   },
   boxImage: {
     height: 100,
@@ -240,4 +155,3 @@ const styles = StyleSheet.create({
     right: 30,
   },
 });
-
